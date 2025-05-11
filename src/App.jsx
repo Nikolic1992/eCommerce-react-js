79d97f8ae6d594a13a2d9a1b4d3bb8f43e1f2e51
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-05-11 13:53:16 +1000

    initial package setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>eCommerce Website</div>;
+  return (
+    <div className="text-3xl flex justify-center text-blue-600 bg-amber-700">
+      eCommerce Website
+    </div>
+  );
 }
 
 export default App;
