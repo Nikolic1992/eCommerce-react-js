e3b0a6738a1033d072669dd1f1297669b1a8463a
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-05-18 12:10:45 +1000

    feat: update API to dummyjson

--- src/components/CategoryComponent.jsx
+++ src/components/CategoryComponent.jsx
@@ -37,10 +37,10 @@ function CategoryComponent() {
         {isLoading ? (
           <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-[10px]">
             {toggleCategory &&
-              allCategory.map((cat) => {
+              allCategory.map((cat, index) => {
                 return (
                   <li
-                    key={cat.id}
+                    key={index}
                     className="w-[200px] bg-mainBlue text-textWhite text-center rounded-lg px-[16px] py-[8px] hover:bg-mainYellow transition-all duration-500 cursor-pointer"
                   >
                     {cat.name}
